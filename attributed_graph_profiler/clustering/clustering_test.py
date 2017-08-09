69b39ecfbb8ab74d4f74b907a1a0cfa41fb9cf65
izio7 <mauriziocasciano7@gmail.com>
2017-08-09 15:47:32 +0200

    SET query

--- attributed_graph_profiler/clustering/clustering_test.py
+++ attributed_graph_profiler/clustering/clustering_test.py
@@ -8,6 +8,8 @@ def main():
     print("Clustering TEST")
     csv_path = "../../resources/dataset_string.csv"
     csv_data_frame = pandas.read_csv(csv_path, delimiter=";")
+    csv_headers = list(csv_data_frame)
+
     print("\nOriginal Values:")
     print(csv_data_frame)
     rows_count, columns_count = csv_data_frame.shape
@@ -42,17 +44,30 @@ def main():
     print("RFDs...")
     for rfd_dic in rfds:
         print("RFD:", rfd_dic)
-        for key, val in groups.items():
-            group = val
+
+        # =============================
+        diff = lambda l1, l2: [x for x in l1 if x not in l2]
+        keys = rfd_dic.keys()
+        print("Keys:", keys)
+        rhs_header_value = diff(list(group), keys)
+        length = len(rhs_header_value)
+        print("RHS_Header:", rhs_header_value[0])
+
+        rfd_dic[rhs_header_value[0]] = rfd_dic.pop("RHS")
+        print("RFD after POP:", rfd_dic)
+
+        if length == 0:
+            print("Lenght:", length)
+            print("Group:", group)
+            print("RFD_dic:", rfd_dic)
+        # =============================
+        for key, group in groups.items():
             print("\nGroup{}\n".format(key), group)
-            diff = lambda l1, l2: [x for x in l1 if x not in l2]
 
-            print("SET{}".format(key))
-            keys = rfd_dic.keys()
-            print("Keys:", keys)
-            rhs_header_value = diff(list(group), keys)
+            query = ' and '.join(['{} <= {}'.format(k, v) for k, v in rfd_dic.items() if not np.isnan(v)])
+            my_set = group.query(query)
 
-            print(group[group[kk] <= rfd_dic[kk]] for kk in keys)
+            print("SET{}\n".format(key), my_set)
 
 
 if __name__ == "__main__":
