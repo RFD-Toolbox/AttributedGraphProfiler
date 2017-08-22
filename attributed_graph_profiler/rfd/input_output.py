c7314ed037ab79d8be3e444e5a4ff86c03163647
izio7 <mauriziocasciano7@gmail.com>
2017-08-22 20:49:17 +0200

    RFD I/O init

---
+++ attributed_graph_profiler/rfd/input_output.py
@@ -0,0 +1,9 @@
+class RFDInputOutput:
+    na_rep = "?"
+    '''
+    Missing data representation symbol.
+    '''
+    encoding = "utf-8"
+    '''
+    The encoding of the input/output file.
+    '''
--- attributed_graph_profiler/rfd_extractor.py
+++ attributed_graph_profiler/rfd_extractor.py
@@ -366,7 +366,8 @@ class RFDExtractor:
                 # start save on file
                 df = pd.DataFrame(rfd_data_frame)
                 filename = rhs_column[0] + ".csv"
-                df.to_csv(filename, index=False, encoding='utf-8', na_rep="?")  #Index if we want index for row
+                df.to_csv(filename, index=False, encoding='utf-8', na_rep="?",
+                          sep=";")  # Index if we want index for row
 
                 # end save on file
                 if sort:
