431bfe9796dc3ac0136de4b338a01258f10f9561
izio7 <mauriziocasciano7@gmail.com>
2017-08-22 16:45:02 +0200

    RFD_DataFrame RHS column rename

--- attributed_graph_profiler/dbms.py
+++ attributed_graph_profiler/dbms.py
@@ -8,7 +8,11 @@ from attributed_graph_profiler.rfd_extractor import RFDExtractor
 def main():
     args = ["-c", "../resources/dataset.csv"]
     rfd_extractor = RFDExtractor(args, False)
-    rfds = rfd_extractor.get_sort_rfd_dictionary_list(True, ["RHS", "weight"])
+    rfds = rfd_extractor.get_sort_rfd_dictionary_list(True, ["RHS"])
+
+    print("\n# of RFDs:", len(rfds))
+    for rfd in rfds:
+        print(rfd)
 
 
 if __name__ == "__main__":
