0bc8a56055e88378834cb11b3a7a4682bfbe2323
Maurizio <mauriziocasciano7@gmail.com>
2019-02-02 14:42:59 +0100

    +PandasTableModel (with sorting)

--- ui/TabsWidget.py
+++ ui/TabsWidget.py
@@ -1,9 +1,15 @@
 import csv
 import typing
+
+from PyQt5.QtCore import QAbstractTableModel
+from pandas import DataFrame
+
 from query_rewriter.io.csv.csv_parser import CSVParser
+from query_rewriter.io.csv.io import CSVInputOutput
+from ui.PandasTableModel import PandasTableModel
 
 from PyQt5.QtWidgets import QTabWidget, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QFormLayout, QLineEdit, \
-    QGroupBox, QLabel, QPushButton, QHBoxLayout, QGridLayout, QComboBox
+    QGroupBox, QLabel, QPushButton, QHBoxLayout, QGridLayout, QComboBox, QTableView
 
 from query_rewriter.query.relaxer import QueryRelaxer
 
@@ -25,7 +31,7 @@ class TabsWidget(QTabWidget):
         self.addTab(self.relax_tab, "RFDs")
 
     def init_dataset_tab(self, path: str):
-        with open(path) as csv_file:
+        '''with open(path) as csv_file:
             sniffer = csv.Sniffer()
             dialect = sniffer.sniff(csv_file.readline())
             csv_file.seek(0)
@@ -40,39 +46,48 @@ class TabsWidget(QTabWidget):
             print("Delimiter: " + dialect.delimiter)
             print("Has Header: " + str(has_header))
             print("Rows: " + str(rows))
-            print("Columns: " + str(columns))
+            print("Columns: " + str(columns))'''
 
-            table = QTableWidget(rows, columns)
-            table.setEnabled(True)
-            table.setRowCount(rows - 1)  # excluding the header from the count
-            table.setColumnCount(columns)
+        '''table = QTableWidget(rows, columns)
+        table.setEnabled(True)
+        table.setRowCount(rows - 1)  # excluding the header from the count
+        table.setColumnCount(columns)
 
-            header = next(reader)
-            print("Header: ")
-            print(header)
+        header = next(reader)
+        print("Header: ")
+        print(header)
 
-            # write the header
+        # write the header
+        for col in range(0, columns):
+            table.setHorizontalHeaderItem(col, QTableWidgetItem(header[col]))
+
+        # write the rows
+        data_row = 0
+        for row in reader:
             for col in range(0, columns):
-                table.setHorizontalHeaderItem(col, QTableWidgetItem(header[col]))
+                table.setItem(data_row, col, QTableWidgetItem(row[col]))
+            data_row += 1
+        '''
 
-            # write the rows
-            data_row = 0
-            for row in reader:
-                for col in range(0, columns):
-                    table.setItem(data_row, col, QTableWidgetItem(row[col]))
-                data_row += 1
+        print("Path: " + path)
 
-            table.setSortingEnabled(True)
+        csv_io: CSVInputOutput = CSVInputOutput()
+        self.data_frame: DataFrame = csv_io.load(path)
+        table = QTableView()
+        pandas_model: QAbstractTableModel = PandasTableModel(self.data_frame, self.dataset_tab)
+        table.setModel(pandas_model)
+        table.setSortingEnabled(True)
 
-            layout = QVBoxLayout()
-            table.setLayout(layout)
+        layout = QVBoxLayout()
+        table.setLayout(layout)
 
-            tab_layout = self.dataset_tab.layout()
+        tab_layout = self.dataset_tab.layout()
 
-            for i in reversed(range(tab_layout.count())):
-                tab_layout.itemAt(i).widget().deleteLater()
+        for i in reversed(range(tab_layout.count())):
+            tab_layout.itemAt(i).widget().deleteLater()
 
-            tab_layout.addWidget(table)
+        tab_layout.addWidget(table)
+
 
     def init_query_tab(self, path: str):
         self.csv_parser = CSVParser(path)
@@ -107,7 +122,7 @@ class TabsWidget(QTabWidget):
             self.line_combos[h] = combo
             self.line_edits[h] = QLineEdit()
 
-            combo.currentTextChanged.connect(lambda ix, key=h, select=combo: self.combo_changed(select, key))
+            combo.currentTextChanged.connect(lambda ix, key=h, select=combo: combo_changed(self, select, key))
             combo.setCurrentIndex(1)
             combo.setCurrentIndex(0)
 
@@ -126,43 +141,46 @@ class TabsWidget(QTabWidget):
         box2.setLayout(box_layout)
 
         query_button = QPushButton("Query", box2)
-        query_button.clicked.connect(self.query_click)
+        query_button.clicked.connect(lambda checked: query_click(self, checked=checked))
 
         tab_layout.addWidget(box2)
 
-    def query_click(self):
-        print("Clicked")
-        operators: dict[str, str] = {}
-        values: dict[str, str] = {}
 
-        for header, line in self.line_edits.items():
-            operators[header] = self.line_combos[header]
-            values[header] = line.text()
-            print(header + " " + self.line_combos[header].currentText() + " " + line.text())
+def query_click(self, checked):
+    print("Clicked")
+    operators: dict[str, str] = {}
+    values: dict[str, str] = {}
 
-        print("OriginalQuery: ", values)
-        original_query_expression = QueryRelaxer.query_dict_to_expr(values)
-        print("OriginalQuery expr: ", original_query_expression)
-        original_query_result_set = self.csv_parser.data_frame.query(original_query_expression)
-        print("Original Query Result Set:\n", original_query_result_set)
+    for header, line in self.line_edits.items():
+        operators[header] = self.line_combos[header]
+        values[header] = line.text()
+        print(header + " " + self.line_combos[header].currentText() + " " + line.text())
 
-    def combo_changed(self, combo: QComboBox, key: str):
-        print("Combo Changed: " + key)
-        print("Combo Changed: " + combo.currentText())
+    print("OriginalQuery: ", values)
+    original_query_expression = QueryRelaxer.query_dict_to_expr(values)
+    print("OriginalQuery expr: ", original_query_expression)
+    original_query_result_set = self.csv_parser.data_frame.query(original_query_expression)
+    print("Original Query Result Set:\n", original_query_result_set)
 
-        if combo.currentText() == "=":
-            self.line_edits[key].setPlaceholderText("The exact value of this property")
-        elif combo.currentText() == "~":
-            self.line_edits[key].setPlaceholderText("A substring of this property")
-        elif combo.currentText() == "∈":
-            self.line_edits[key].setPlaceholderText("A set of values for this property. e.g. {Value_1, Value_2, Value_3} or [Value_min, Value_MAX]")
-        elif combo.currentText() == ">":
-            self.line_edits[key].setPlaceholderText("A minimum value of this property (value excluded)")
-        elif combo.currentText() == ">=":
-            self.line_edits[key].setPlaceholderText("A minimum value of this property (value included)")
-        elif combo.currentText() == "<":
-            self.line_edits[key].setPlaceholderText("A maximum value of this property (value excluded)")
-        elif combo.currentText() == "<=":
-            self.line_edits[key].setPlaceholderText("A maximum value of this property (value included)")
-        elif combo.currentText() == "!=":
-            self.line_edits[key].setPlaceholderText("A value not admitted for this property")
+
+def combo_changed(self, combo: QComboBox, key: str):
+    print("Combo Changed: " + key)
+    print("Combo Changed: " + combo.currentText())
+
+    if combo.currentText() == "=":
+        self.line_edits[key].setPlaceholderText("The exact value of this property")
+    elif combo.currentText() == "~":
+        self.line_edits[key].setPlaceholderText("A substring of this property")
+    elif combo.currentText() == "∈":
+        self.line_edits[key].setPlaceholderText(
+            "A set of values for this property. e.g. {Value_1, Value_2, Value_3} or [Value_min, Value_MAX]")
+    elif combo.currentText() == ">":
+        self.line_edits[key].setPlaceholderText("A minimum value of this property (value excluded)")
+    elif combo.currentText() == ">=":
+        self.line_edits[key].setPlaceholderText("A minimum value of this property (value included)")
+    elif combo.currentText() == "<":
+        self.line_edits[key].setPlaceholderText("A maximum value of this property (value excluded)")
+    elif combo.currentText() == "<=":
+        self.line_edits[key].setPlaceholderText("A maximum value of this property (value included)")
+    elif combo.currentText() == "!=":
+        self.line_edits[key].setPlaceholderText("A value not admitted for this property")
